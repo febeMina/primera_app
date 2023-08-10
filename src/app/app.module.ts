bf31935de583dd43be1b2cc3349a754882bf0d05
Febe Martinez <febemina5@gmail.com>
2023-08-09 19:56:19 -0600

    Ultimos cambios con los componentes padre e Hijo 090823

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,13 +5,17 @@ import { AppComponent } from './app.component';
 import { EmpleadosComponent } from './empleados/emplados.component';
 import { EmpleadoComponent } from './empleado/empleado.component';
 import { EmpleadoFeoComponent } from './empleado-feo/empleado-feo.component';
+import { PadreComponent } from './padre/padre.component';
+import { HijoComponent } from './hijo/hijo.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     EmpleadosComponent,
     EmpleadoComponent,
-    EmpleadoFeoComponent
+    EmpleadoFeoComponent,
+    PadreComponent,
+    HijoComponent
   ],
   imports: [
     BrowserModule
